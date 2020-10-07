e5be859e9514e320af25ec6e7989c64feb45efb3
Murodov Azizmurod <65248559+murodovazizmurod@users.noreply.github.com>
2020-10-07 10:08:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def result(message):
     if message.chat.type == "group" or message.chat.type == "supergroup":
         if bot.get_chat_member(message.chat.id, message.from_user.id).status == "administrator" or bot.get_chat_member(message.chat.id, message.from_user.id).status == "creator":
             try:
+		db.create(message.chat.id)
                 if len(db.results(message.chat.id)) > 0:
                     bot.send_message(message.chat.id, text=db.results(message.chat.id))
                 else:
@@ -42,7 +43,10 @@ def result(message):
                 bot.send_message(message.chat.id, "Xatolik yuz berdi!\nKod: 3")
 @bot.message_handler(content_types=['left_chat_member'])
 def message_left_member(message):
-	bot.delete_message(message.chat.id, message.message_id)
+	try:
+		bot.delete_message(message.chat.id, message.message_id)
+	except:
+		bot.send_message(message.chat.id, text="Botga administratorlik huquqi berilgan bo'lishi lozim!")
 	
 
 if __name__ == "__main__":
